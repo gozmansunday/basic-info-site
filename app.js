aae0f8ad1ec2394f08fdf26598e81a0ccc5f86b5
Chiagoziem Sunday <gozmansunday@gmail.com>
2024-08-16 11:59:22 +0100

    feat: install express and create express version of server.js

---
+++ app.js
@@ -0,0 +1,55 @@
+import express from "express";
+import fs from "fs/promises";
+import path from "path";
+import url from "url";
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+// Get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const filePathMiddleware = async (req, res, fileName, next) => {
+  const filePath = path.join(__dirname, "public", fileName);
+  const data = await fs.readFile(filePath);
+
+  next(data);
+};
+
+// Homepage
+app.get("/", async (req, res) => {
+  filePathMiddleware(req, res, "index.html", (data) => {
+    res.status(200).type("html").send(data);
+  });
+});
+
+// About page
+app.get("/about", async (req, res) => {
+  filePathMiddleware(req, res, "about.html", (data) => {
+    res.status(200).type("html").send(data);
+  });
+});
+
+// Contact page
+app.get("/contact", async (req, res) => {
+  filePathMiddleware(req, res, "contact.html", (data) => {
+    res.status(200).type("html").send(data);
+  });
+});
+
+// 404 page
+app.get("/:route", async (req, res) => {
+  const route = req.params.route;
+
+  if (!(route === "/" || route === "/about" || route === "/contact")) {
+    filePathMiddleware(req, res, "404.html", (data) => {
+      res.status(404).type("html").send(data);
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Basic Info Site - Listening on PORT ${PORT}!`);
+});
